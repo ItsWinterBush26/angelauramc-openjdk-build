22f1173af4b3d421234c17edf44c636fe0168243
ItsWinterBush <154499206+ItsWinterBush26@users.noreply.github.com>
2025-08-24 12:35:15 +0800

    Update 5_clonejdk.sh
    Signed-off-by: ItsWinterBush  <154499206+ItsWinterBush26@users.noreply.github.com>

--- 5_clonejdk.sh
+++ 5_clonejdk.sh
@@ -3,10 +3,10 @@ set -e
 
 if [[ $TARGET_VERSION -eq 21 ]]; then
     if [[ $BUILD_IOS ]]; then
-        git clone --branch jdk-21.0.1+12.1 --depth 1 https://github.com/adoptium/jdk21u.git openjdk-21
+        git clone --branch release --depth 1 https://github.com/adoptium/jdk21u.git openjdk-21
     else
         # TODO: Update Android to JDK 21.0.1+12
-        git clone --branch jdk21.0.1+12.1 --depth 1 https://github.com/openjdk/jdk21u openjdk-21
+        git clone --branch release --depth 1 https://github.com/openjdk/jdk21u openjdk-21
     fi
 elif [[ $TARGET_VERSION -eq 17 ]]; then
     git clone --depth 1 https://github.com/openjdk/jdk17u openjdk-17
